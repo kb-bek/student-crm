16de80ec45a4b693e8f504392d06128581f99f49
Kubanychbek Talipov <talipovkuba555@gmail.com>
2024-06-28 15:20:11 +0600

    First commit - Added first version of project

---
+++ src/main/java/com/kaitech/student_crm/StudentCrmApplication.java
@@ -0,0 +1,13 @@
+package com.kaitech.student_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrmApplication.class, args);
+	}
+
+}
