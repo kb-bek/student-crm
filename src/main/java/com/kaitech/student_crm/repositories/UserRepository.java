d7c63ebcabf5b7434e69668e708aeaa5b199576b
Kubanychbek Talipov <talipovkuba555@gmail.com>
2024-06-30 12:33:19 +0600

    Added User Repository

---
+++ src/main/java/com/kaitech/student_crm/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.kaitech.student_crm.repositories;
+
+import com.kaitech.student_crm.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByUsername(String username);
+
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserById(Long id);
+}
