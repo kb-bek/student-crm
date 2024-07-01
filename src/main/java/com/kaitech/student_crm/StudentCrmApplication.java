cb6ca911da693471cad955b0e5fba6f9ee60046f
Kubanychbek Talipov <talipovkuba555@gmail.com>
2024-07-01 12:48:22 +0600

    Added bean model mapper

--- src/main/java/com/kaitech/student_crm/StudentCrmApplication.java
+++ src/main/java/com/kaitech/student_crm/StudentCrmApplication.java
@@ -1,7 +1,9 @@
 package com.kaitech.student_crm;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StudentCrmApplication {
@@ -10,4 +12,8 @@ public class StudentCrmApplication {
 		SpringApplication.run(StudentCrmApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
