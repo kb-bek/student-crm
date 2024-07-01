8e62f01b76b321516e5ba867850a89a6fa890fba
Kubanychbek Talipov <talipovkuba555@gmail.com>
2024-07-01 12:44:21 +0600

    Added Student repository

---
+++ src/main/java/com/kaitech/student_crm/repositories/StudentUserRepository.java
@@ -0,0 +1,18 @@
+package com.kaitech.student_crm.repositories;
+
+import com.kaitech.student_crm.models.User;
+import com.kaitech.student_crm.models.roles.ERole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface StudentUserRepository extends JpaRepository<User, Long> {
+
+    List<User> findByRolesContaining(ERole role);
+
+    Optional<User> findUserById(Long id);
+}
